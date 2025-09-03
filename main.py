f4ed571f533a0b2d7708479ffbb0a5e52ff57305
Ali Trabelsi <atrabelsi@netbuilder.io>
2025-09-03 12:27:00 +0100

    first attempt at 1 million records

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("hello")
+one_million = open('measurements_1m.txt', 'r')
+
+cities = {}
+
+for row in one_million:
+    entry = row.strip().split(";")
+    city = entry[0]
+    temp = float(entry[1])
+
+    if city in cities:
+        if cities[city]['min'] > temp:
+            max = cities[city]['max']
+            cities.update({city : {'min': temp, 'max': max}})
+
+        if cities[city]['max'] < temp:
+            min = cities[city]['min']
+            cities.update({city : {'min': min, 'max': temp}})
+
+        
+    else:
+        cities.update({city : {'min': temp, 'max': temp}})
+
+for key, value in cities.items():
+    print(key, value)
+
